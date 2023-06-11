248fc90371b1ef920cd98a07ddab14192d4bb913
madhuri <madhuriaileni2320@gmail.com>
2023-06-11 12:00:39 +0530

    about page

--- src/components/store/CardProvider.js
+++ src/components/store/CardProvider.js
@@ -3,7 +3,7 @@ import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
   const [items, setItems] = useState([]);
-  const [productObj, setProductObj] = useState({});
+ const [productObj, setProductObj] = useState({});
   const userLoginDetails = localStorage.getItem("logIn");
   const [token, setToken] = useState(userLoginDetails);
   const userEmail = localStorage.getItem("email");
@@ -13,7 +13,7 @@ const CartProvider = (props) => {
   useEffect(() => {
     const asyncFun = async () => {
       let response = await fetch(
-        `https://crudcrud.com/api/10731e34a19046a9b8f5646f56ac9e22/items${cartContext.email}`
+        `https://crudcrud.com/api/fbe8e7e3a9a94caba9cdc0374ced6af9/cart${cartContext.email}`
       );
       let data = await response.json();
       console.log(data);
@@ -31,7 +31,7 @@ const CartProvider = (props) => {
     if (existingItem === -1) {
       // If there is no existing item it will return -1
       let response = await fetch(
-        `https://crudcrud.com/api/10731e34a19046a9b8f5646f56ac9e22/items${cartContext.email}`,
+        `https://crudcrud.com/api/fbe8e7e3a9a94caba9cdc0374ced6af9/cart${cartContext.email}`,
         {
           method: "POST",
           body: JSON.stringify(item),
@@ -52,7 +52,7 @@ const CartProvider = (props) => {
       console.log(updatedItem);
 
       let response = await fetch(
-        `https://crudcrud.com/api/10731e34a19046a9b8f5646f56ac9e22/items${cartContext.email}/${allItems[existingItem]._id}`,
+        `https://crudcrud.com/api/fbe8e7e3a9a94caba9cdc0374ced6af9/cart${cartContext.email}/${allItems[existingItem]._id}`,
 
         {
           method: "PUT",
@@ -77,7 +77,7 @@ const CartProvider = (props) => {
     console.log(item._id);
     if (item.quantity <= 1) {
       const response = await fetch(
-        `https://crudcrud.com/api/10731e34a19046a9b8f5646f56ac9e22/items${cartContext.email}/${item._id}`,
+        `https://crudcrud.com/api/fbe8e7e3a9a94caba9cdc0374ced6af9/cart${cartContext.email}/${item._id}`,
         { method: "DELETE" }
       );
       const updatedItems = items.filter((itm) => {
@@ -88,7 +88,7 @@ const CartProvider = (props) => {
     } else {
       item.quantity = item.quantity - 1;
       let response = await fetch(
-        `https://crudcrud.com/api/10731e34a19046a9b8f5646f56ac9e22/items${cartContext.email}/${item._id}`,
+        `https://crudcrud.com/api/fbe8e7e3a9a94caba9cdc0374ced6af9/cart${cartContext.email}/${item._id}`,
         {
           method: "PUT",
           body: JSON.stringify({

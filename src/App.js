747ed7008cfaf3b1006da4ee4171d12d9c4393b7
madhuri <madhuriaileni2320@gmail.com>
2023-07-05 10:46:56 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import './App.css';
-import React from 'react';
-import { Routes,Route} from "react-router-dom";
+import React,{useContext} from 'react';
+import { Switch,Route,Redirect} from "react-router-dom";
 import RootLayout from './components/RootLayout/RootLayout';
 import HomePage from './components/HomePage/HomePage';
 import ListItems from './components/Items/ListItems';
 import ProductPage from './components/Items/ProductPage';
 import AboutPage from './components/AboutPage/AboutPage';
 import ContactUs from './components/ContactUs/Contactus';
+import Auth from './Authentication/Auth';
+import CartContext from './components/store/cart-context';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 
 const App=()=>{
+  const ctx=useContext(CartContext);
   return (
       <RootLayout>
-       <Routes>
-          <Route path="/" exact element={<HomePage/>} />
-          <Route path="/home" exact element={<HomePage/>}/>
-          <Route path="/store" exact element={<ListItems/>}/>
-          <Route path="/store/:productId" element={<ProductPage/>}/>
-          <Route path="/about" element={<AboutPage/>}/>
-          <Route path="/contactus" element={<ContactUs/>}/>
-       </Routes>
+       <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home"/>
+          </Route>
+          <Route path="/home" exact>
+             <HomePage/>
+          </Route>
+          <Route path="/store" exact>
+             {ctx.isLoggedIn && <ListItems/>}
+             {!ctx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>  
+          <Route path="/store/:productId">
+             <ProductPage/>
+          </Route>
+          <Route path="/about">
+            <AboutPage/>
+          </Route>
+          <Route path="/contactus">
+            <ContactUs/>
+          </Route>
+          <Route path="/auth">
+            <Auth/>
+          </Route>
+          <Route path="*">
+            <ErrorPage/>
+          </Route>
+       </Switch>
       </RootLayout>
 
 
